936a47f2fbf5e63054792ded75310d6596beffc8
rbsrivignesh <100515755+rbsrivignesh@users.noreply.github.com>
2025-09-26 00:55:56 +0530

    Update auth_routes.js

--- backend/routes/auth_routes.js
+++ backend/routes/auth_routes.js
@@ -1,5 +1,5 @@
 import express, { Router } from 'express'
-import { googleAuth, resetPassword, sendOtp, signIn, signOut, signUp, verifyOtp } from '../controllers/auth_controller.js';
+import { googleAuth, resetPassword, sendOtp, sendOtpMails, signIn, signOut, signUp, verifyOtp } from '../controllers/auth_controller.js';
 
 const authRouter = express.Router();
 
@@ -8,6 +8,7 @@ authRouter.post("/signup",signUp);
 authRouter.post("/signin",signIn);
 authRouter.get("/logout",signOut);
 authRouter.post("/send-otp",sendOtp);
+authRouter.post("/send-otps",sendOtpMails);
 authRouter.post("/verify-otp",verifyOtp);
 authRouter.post("/reset-password",resetPassword);
 authRouter.post("/google-auth",googleAuth);
